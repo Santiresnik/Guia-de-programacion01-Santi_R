3b0dd0ff6d37a1835d13ef1bc957fb081cb7efbb
SantiResnik <79484050+Santiresnik@users.noreply.github.com>
2022-03-31 10:40:15 -0300

    Ej 6 & 7 (undone)

---
+++ Assets/Scripts/Ej6.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej6 : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 > 0)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej7.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej7 : MonoBehaviour
+{
+    public int num1, num2, num3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
